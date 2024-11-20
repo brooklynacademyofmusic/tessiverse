caf3aa7632beb3a2075dfcbe6aa98133c2ebf571
Sky Syzygy <ssyzygy@bam.org>
2024-11-20 16:46:52 -0500

    Work on authentication flow

--- app/src/routes/+layout.svelte
+++ app/src/routes/+layout.svelte
@@ -1,17 +1,18 @@
 <script lang="ts">
 	import "../app.css"
-	let { children } = $props();
 	import { Button } from "$lib/components/ui/button";
 	import DarkLight from "$lib/components/darklight.svelte";
 	import Github from "$lib/components/github.svelte";
 	import { ModeWatcher } from "mode-watcher";
+
 	let repo = "skysyzygy/tessiverse"
+	let { children } = $props();
 </script>
 
 <ModeWatcher />
 <div class="dark:bg-black bg-white flex min-h-screen w-full flex-col">
-	<nav class="hidden md:flex gap-2 p-1 bg-secondary w-full justify-end">
-	  <Button size="sm" class="bg-secondary border-secondary-foreground" variant="outline">Logout</Button>
+	<nav class="hidden sticky md:flex gap-2 p-1 bg-secondary w-full justify-end">
+	  <Button size="sm" href="/.auth/logout" class="bg-secondary border-secondary-foreground" variant="outline">Logout</Button>
 	  <DarkLight />
 	  <Github repo={repo} />
 	</nav>
--- app/src/routes/+page.server.ts
+++ app/src/routes/+page.server.ts
@@ -1,9 +1,31 @@
-
 import type { PageServerLoad } from './$types';
+import { UserConfig } from '$lib/config';
+import { User } from 'lucide-svelte';
+import { error } from '@sveltejs/kit'
 
-export const load: PageServerLoad = () => {
-    
+type clientPrincipal = {
+    identityProvider: string,
+    userId: string,
+    userDetails: string,
+    userRoles: string[]
+} 
+
+export const load: PageServerLoad = async ( { request, cookies }) => {
+    const header = Buffer.from(
+                        request.headers.get('x-ms-client-principal') || 
+                        cookies.get("StaticWebAppsAuthCookie") || 
+                        "", 'base64');
+    const user = JSON.parse(header.toString('ascii') || "{}") as clientPrincipal
+
+    if (!user.userDetails) {
+        error(401, 'User not logged in!?')
+    }
+
+    const userConfig = new UserConfig(user.userDetails)
+    console.log(await userConfig.loadFromAzure())
+
     return {
-        name: new Promise((resolve) => {setTimeout(() => resolve("Sky"), 1000)})
+        name: user.userDetails,
+        config: null
     }
 }
