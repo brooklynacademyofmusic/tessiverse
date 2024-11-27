e1453022decb9fc72177a76890516cdccd54ac03
skysyzygy <54546783+skysyzygy@users.noreply.github.com>
2024-11-27 11:31:29 -0500

    Playing with switch on appCard

--- src/lib/components/appCard.svelte
+++ src/lib/components/appCard.svelte
@@ -1,23 +1,22 @@
 <script lang="ts">
-    import { Card, CardTitle, CardHeader, CardContent, CardDescription, CardFooter } from "./ui/card"
+    import * as Card from "./ui/card"
     import { Button } from "./ui/button"
     import { Settings } from "lucide-svelte"
+    import { Switch } from "./ui/switch"
     import { cn } from "../utils"
     let { title, children, disabled = false, class: className = undefined } = $props()
-    
-    if (disabled) {
-        var blur = "blur-sm opacity-50"
-    }
+    var blur = $derived(
+        disabled ? "blur-sm opacity-50" : ""
+    )
 </script>
 
-<Card class={cn("flex flex-col border-primary shadow-3xl shadow-primary",blur,className)}>
+<Card.Root class={cn("flex flex-col border-primary shadow-3xl shadow-primary",blur,className)}>
     <div class="w-full rounded-t bg-secondary p-3 font-extralight tracking-tight">{title}</div>
-    <CardContent class="grow flex flex-col">{@render children()}</CardContent>
+    <Card.CardContent class="grow flex flex-col">{@render children()}</Card.CardContent>
     <div class="text-right">
-        {#if !disabled}
+        <Switch bind:checked={disabled} />
         <Button size="icon" variant="ghost" class="text-secondary-foreground border-primary border-r-0 border-b-0">
             <Settings class="w-5 h-5"/>
         </Button>
-        {/if}
     </div>
-</Card>
+</Card.Root>
