8f60da32d1a8e330d31178bdb3cebacce14e64a9
skysyzygy <54546783+skysyzygy@users.noreply.github.com>
2024-12-10 10:07:18 -0500

    Implementing tessitura form

--- src/routes/test/+page.svelte
+++ src/routes/test/+page.svelte
@@ -1,25 +1,16 @@
 <script lang="ts">
+    import type { PageData } from "./$types"
     import Tessitura from '$lib/apps/tessitura/tessitura.svelte'
     import { superForm, type SuperForm, type Infer } from "sveltekit-superforms";
     import { zodClient } from "sveltekit-superforms/adapters";
     import { tessituraSchema } from "$lib/apps/tessitura/tessitura.schema"
 
-    let {servers = [],
-        groups = [{value: "PlanMem", label: "Membership (full-time)"}],
-        user = {
-            userid: "ssyzygy",
-            tessiApiUrl: "https://tessi-db-prd1",
-            group: "PlanMem",
-            password: ""
-        }} = $props();
 
-    const form: SuperForm<Infer<typeof tessituraSchema>> = superForm(user, {
-		validators: zodClient(tessituraSchema),
-	});
-    const {form: formData} = form
-    $effect(() => {
-        groups.push({value: $formData.userid, label: $formData.userid})
-    })
+    let { data }: { data: PageData } = $props()
+
 </script>
-
-<Tessitura form={form} action="/user?/login" servers={servers} groups={groups}/>
+{#await data.appData.tessitura then tessitura}
+<Tessitura form={superForm({userid: tessitura.userid || "", group: tessitura.group || "", tessiApiUrl: tessitura.tessiApiUrl, password: ""}, {
+		validators: zodClient(tessituraSchema),
+	})} action={tessitura.key} servers={tessitura.servers} groups={tessitura.groups}/>
+{/await}
