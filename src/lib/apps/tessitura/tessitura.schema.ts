1c2fa6ad17b14469df6e1b24385cfbe81e014d3f
skysyzygy <54546783+skysyzygy@users.noreply.github.com>
2024-12-15 12:29:27 -0500

    Limiting tessiApiUrl to valid values

--- src/lib/apps/tessitura/tessitura.schema.ts
+++ src/lib/apps/tessitura/tessitura.schema.ts
@@ -1,12 +1,7 @@
 import { z } from "zod";
-export const servers = [
-  {value: "https://tessi-db-prd1", label: "TESSI-DB-PRD1/Impresario"},
-  {value: "https://t-gw-test-b-ex-rest.bam.org/TessituraService", label: "TESSI-TEST-B/Impresario"}
-]  
-export type ValidServerValues = {
-  [K in keyof typeof servers]: typeof servers[K] extends {value: infer I} ? I : never
-}
-export const validServerValues = Object.fromEntries(servers.map((e) => [e.label, e.value]))
+import * as config from '$lib/const'
+
+const validServerValues = Object.fromEntries(config.servers.map((e) => [e.label, e.value]))
 
 export const tessituraSchema = z.object({
   tessiApiUrl: z.nativeEnum(validServerValues),
