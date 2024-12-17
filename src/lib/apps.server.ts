e324631c7868bae78e328f5d38b3c44c43522e55
Sky Syzygy <ssyzygy@bam.org>
2024-12-17 18:08:57 -0500

    Split apps and baseapp into separate files b/c of circular dependencies

--- src/lib/apps.server.ts
+++ src/lib/apps.server.ts
@@ -1,31 +1,12 @@
-import { UserLoaded, type Backend } from '$lib/azure'
+import { type Backend } from '$lib/azure'
 import { type ActionFailure } from '@sveltejs/kit'
-import { BaseApp, type App } from './apps'
-import type { Apps } from './config'
 
-export interface AppServer<In extends object, Out extends object> {
-    data: App<In>
+export interface AppServer<In extends {key: string}, Out extends object> {
+    data: In
     // function to load and process data from the backend
     load(backend: Backend<any>): Promise<In>
     // function to save data to the backend
-    save(backend: Backend<any>, data: Out): Promise<void | ActionFailure<any> | {form: any}>    
+    save(backend: Backend<any>, data: In): Promise<void | ActionFailure<any> | {form: any}>    
 }
 
-export abstract class BaseAppServer implements AppServer<any,any> {
-    data = new BaseApp()
-    key: keyof Apps
-
-    constructor() {
-        this.key = this.data.key as keyof Apps
-    }
-
-    async load(backend: UserLoaded): Promise<any> {
-        Object.assign(this.data,backend.load({identity: backend.identity, app: this.key}))
-        return this
-    }
-
-    async save(backend: UserLoaded, data: any): Promise<void | ActionFailure<any> | {form: any}> {
-        return backend.save({identity: backend.identity, app: this.key}, data)
-    }
-}
 
