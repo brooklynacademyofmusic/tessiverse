c7a2123027273304db45349796deccab4e2855b6
Sky Syzygy <ssyzygy@bam.org>
2024-12-17 18:11:52 -0500

    Add planStep types

--- src/lib/apps/planStep/planStep.ts
+++ src/lib/apps/planStep/planStep.ts
@@ -1,3 +1,4 @@
+import type { Serializable } from '$lib/apps'
 import { type Component } from 'svelte'
 
 let PlanStep: Component<any> = {} as Component<any>
@@ -10,3 +11,6 @@ export class PlanStepApp {
     stepType = 0
     closeStep = true
 }
+
+export type PlanStepAppLoad = Serializable<PlanStepApp>
+export type PlanStepAppSave = PlanStepAppLoad
