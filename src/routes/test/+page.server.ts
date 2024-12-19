b3888af597e5e44ead2b2acf574bbb506455ee97
Sky Syzygy <ssyzygy@bam.org>
2024-12-19 18:58:24 -0500

    Build out of test page

--- src/routes/test/+page.server.ts
+++ src/routes/test/+page.server.ts
@@ -1,18 +1,40 @@
 import type { PageServerLoad } from "./$types"
+import { env } from '$env/dynamic/private'
+import { Azure } from "$lib/azure"
+import fs from 'node:fs/promises'
+import child_process from 'node:child_process'
+import * as config from '$lib/const'
+import { tq } from "$lib/tq"
+import { User } from "$lib/user"
+import { TessituraAppServer } from "$lib/apps/tessitura/tessitura.server"
 
-type TestResult = {
-    status: boolean
-    error: any
-}
-
-export const load: PageServerLoad = async () => {
+export const load: PageServerLoad = ({fetch}) => {
     return {
-        "Azure key vault access": true,
-        "tq exists": false,
-        "tq is executable": false,
-        "tq admin is configured": false,
-        "can reach Tessitura": false,
-        "tq admin is valid": false,
-        "can query Tessitura": false
-    } as Record<string,TestResult>
+        "AZURE_KEY_VAULT_URL defined": env.AZURE_KEY_VAULT_URL ? true : 
+            "The AZURE_KEY_VAULT_URL environment variable must be set",
+        "Azure key vault access": (async () => {new Azure()})().then(() => true)
+            .catch((e) => e),
+        "tq exists": fs.stat("bin/tq").then(() => true).catch((e) => JSON.parse(JSON.stringify(e))),
+        "tq is executable": (async () => {
+            let tq = await child_process.spawnSync("bin/tq")
+            if(tq.error) 
+                throw(JSON.parse(JSON.stringify(tq.error)))
+            })().then(() => true).catch((e) => e),
+        "Can reach Tessitura": fetch(config.servers[0].value)
+            .then((res) => {if (res.status >= 300) throw(res)})
+            .then(() => true)
+            .catch((e) => JSON.parse(JSON.stringify(e))),
+        "TQ_ADMIN_LOGIN defined": env.TQ_ADMIN_LOGIN ? true : 
+            "The TQ_ADMIN_LOGIN environment variable must be set",        
+        "TQ_ADMIN_LOGIN is defined in the key store": tq("auth","list")
+            .then((l: string) => l.includes(env.TQ_ADMIN_LOGIN))
+            .catch((e) => e),
+        "tq admin auth is valid": tq("auth","validate",undefined,undefined,env.TQ_ADMIN_LOGIN)
+            .then(() => true)
+            .catch((e) => e.split("\n")),
+        "Can query Tessitura": tq("get","constituents",undefined,{"constituentid":"1"},
+            env.TQ_ADMIN_LOGIN)
+            .then(() => true)
+            .catch((e) => e)
+    }
 }
--- src/routes/test/+page.svelte
+++ src/routes/test/+page.svelte
@@ -1,16 +1,30 @@
 <script lang="ts">
-    import type { PageData } from "./$types"
+    import type { PageData, PageServerData } from "./$types"
 
-    let { data } : {data: PageData} = $props()
+    let { data } : {data: PageServerData} = $props()
     
 </script>
+<style>
+    :global(main) {
+        gap: 1em !important
+    }
+</style>
+
+{#snippet error(e: any)}
+    <span class="text-red-500">error</span>
+    <br/><code>{JSON.stringify(e)}</code>
+{/snippet}
 
 {#each Object.entries(data) as [key,value]}
     <p><b>{key}</b>: 
-        {#if value}
-            <span class="text-green-500">success!</span>
-        {:else}
-            <span class="text-red-500">error</span>
-        {/if}
-        </p>
+        {#await value then value}
+            {#if value === true}
+                <span class="text-green-500">success!</span>
+            {:else}
+                {@render error(value)}
+            {/if}
+        {:catch e }
+            {@render error(value)}
+        {/await}
+    </p>
 {/each}
