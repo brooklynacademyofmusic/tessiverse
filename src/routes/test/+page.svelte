70fe84807844901a170319de2696127ef90ec9b9
skysyzygy <54546783+skysyzygy@users.noreply.github.com>
2024-12-19 08:36:28 -0500

    Very simple static test

--- src/routes/test/+page.svelte
+++ src/routes/test/+page.svelte
@@ -1,10 +1,1 @@
-<script lang="ts">
-    import type { PageData } from "./$types"
-    import Tessitura from '$lib/apps/tessitura/tessitura.svelte'
-    
-    let { data }: { data: PageData } = $props()
-
-</script>
-{#await data.appData.tessitura then tessitura}
-<Tessitura data={{userid: tessitura.userid, group: tessitura.group, tessiApiUrl: tessitura.tessiApiUrl, valid: true, key: "tessitura"}} />
-{/await}
+<h1>It works!</h1>
