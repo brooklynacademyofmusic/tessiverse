c71434cafcf84e16d6c9934064491ecd5afed71b
skysyzygy <54546783+skysyzygy@users.noreply.github.com>
2024-12-19 08:34:15 -0500

    Move routes out of the api subdir, which Azure needs

---
+++ src/routes/planStep/+server.ts
@@ -0,0 +1,15 @@
+import type { RequestHandler } from './$types';
+import { planStep, type PlanStepEmail } from '$lib/apps/planStep/planStep.server'
+import { error } from '@sveltejs/kit';
+import * as ERRORS from '$lib/errors'
+
+export const POST: RequestHandler = async ({ request, locals }) => {
+    if (! ("admin" in locals.user.userRoles)) {
+        error(403, ERRORS.AUTH)
+    }
+    return (request.body || new ReadableStream()).getReader().read()
+    .then((body) => JSON.parse(body.value?.toString() || "null") as PlanStepEmail)
+    .then((email) => planStep(email))
+    .then((response) => new Response(response))
+
+}
