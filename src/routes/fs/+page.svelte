e12d3aa1198cce1faf2f715df952f453173a48c0
skysyzygy <54546783+skysyzygy@users.noreply.github.com>
2024-12-19 11:32:27 -0500

    Filesystem explorer

---
+++ src/routes/fs/+page.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import type { PageData } from "./$types";
+
+    let { data } : {data: PageData } = $props()
+
+</script>
+
+<h1>data.dir</h1>
+{#await data.dir then dir}
+    {#each dir as d}
+        <br/>{d}
+    {/each}
+{:catch}
+    Error with data.dir
+{/await}
+
+<h1>data.ls</h1>
+{#await data.tq then ls}
+    {JSON.stringify(ls)}
+{/await}
