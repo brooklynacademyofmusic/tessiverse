fb0643e543f5d20dde1f21b4afc48948947dfec7
skysyzygy <54546783+skysyzygy@users.noreply.github.com>
2024-12-19 13:16:15 -0500

    Setup test scaffold

---
+++ src/routes/test/+page.server.ts
@@ -0,0 +1,10 @@
+export const load = async () => {
+    return {
+        "Key vault access": false,
+        "tq": false,
+        "tq Admin": false,
+        "Tessitura connection": false,
+        "tq Admin valid": false,
+        "Tessitura query": false
+    }
+}
