4ea88e34681a88fff13468b5eac4bfcffcdaf952
skyolson <54546783+skyolson@users.noreply.github.com>
2024-12-23 15:01:58 -0500

    Tighten constraints so that keys must match between app and appserver

--- src/lib/config.server.ts
+++ src/lib/config.server.ts
@@ -2,14 +2,15 @@ import { env } from '$env/dynamic/private'
 import type { AppServer } from './apps.server';
 import { PlanStepAppServer } from './apps/planStep/planStep.server';
 import { TessituraAppServer } from './apps/tessitura/tessitura.server';
+import type { Apps } from '$lib/config';
 
 export const key_vault_url = env.AZURE_KEY_VAULT_URL || "";
 
-type AppServerConstraint<T> = {
+type AppServerConstraint<T = Apps> = {
     [K in keyof T]: K extends infer S ? S extends string ? AppServer<S,any,any> : never : never
 }
 
-export class AppServers implements AppServerConstraint<AppServers> {
+export class AppServers implements AppServerConstraint {
     [k: string]: AppServer<string,any,any>
     tessitura = new TessituraAppServer()
     planStep = new PlanStepAppServer()
--- src/lib/config.ts
+++ src/lib/config.ts
@@ -2,10 +2,10 @@ import type { App } from './apps';
 import { PlanStepApp } from './apps/planStep/planStep';
 import { TessituraApp } from './apps/tessitura/tessitura';
 
-type AppConstraint<T> = {
+type AppConstraint<T = Apps> = {
     [K in keyof T]: K extends infer S ? S extends string ? App<S,any> : never : never
 }
-export class Apps implements AppConstraint<Apps> {
+export class Apps implements AppConstraint {
     tessitura = new TessituraApp()
     planStep = new PlanStepApp()
 } 
