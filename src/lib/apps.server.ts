9f505902bd08a5e234cb5932082db523e12d5096
skysyzygy <54546783+skysyzygy@users.noreply.github.com>
2024-12-24 13:46:26 -0500

    Fix interface definition for backend

--- src/lib/apps.server.ts
+++ src/lib/apps.server.ts
@@ -1,13 +1,13 @@
-import { type Backend } from '$lib/azure'
+import { type Backend, type BackendKey } from '$lib/azure'
 import { type ActionFailure } from '@sveltejs/kit'
 
 export interface AppServer<Key extends string, In extends object, Out extends object> {
     key: Key
     data: In
     // function to load and process data from the backend
-    load(backend: Backend<any>): Promise<In>
+    load(backend: Backend<any>, key: BackendKey): Promise<In>
     // function to save data to the backend
-    save(backend: Backend<any>, data: Out): Promise<void | ActionFailure<any> | {form: any}>    
+    save(data: Out, backend: Backend<any>, key: BackendKey): Promise<void | ActionFailure<any> | {form: any}>    
 }
 
 
