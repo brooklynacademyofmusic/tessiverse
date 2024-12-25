4fb716c10a823ab99c27342942cc42147d0fc186
skysyzygy <54546783+skysyzygy@users.noreply.github.com>
2024-12-25 14:02:50 -0500

    Move test to /tessitura/test

--- src/routes/tessitura/test/+page.server.ts
+++ src/routes/tessitura/test/+page.server.ts
@@ -7,6 +7,7 @@ import fs from 'node:fs/promises'
 import child_process from 'node:child_process'
 import * as config from '$lib/const'
 import { tq } from "$lib/tq"
+import { URL } from "node:url"
 
 const stringify = function(o: any): any {
     console.log(o)
@@ -16,15 +17,16 @@ const stringify = function(o: any): any {
 export const load: PageServerLoad = ({fetch}) => {
 
     let serverTests = config.servers.map((s) => {return{
-        ["Can reach "+s.label]: fetch(s.value)
+        ["Can reach "+s.label]: fetch("https://"+s.value,{ signal: AbortSignal.timeout(15000) })
             .then((res) => {if(res.status >= 300) throw(res)})
             .then(() => true)
             .catch((e) => stringify(e)),
-        ["tq admin auth is valid on "+s.label]: tq("auth","validate",undefined,undefined,env.TQ_ADMIN_LOGIN)
+        ["tq admin auth is valid on "+s.label]: tq("auth","validate",{login: s.value + "|" + env.TQ_ADMIN_LOGIN})
             .then(() => true)
             .catch((e) => stringify(e)),
-        ["Can query "+s.label]: tq("get","constituents",undefined,{"constituentid":"1"},
-            env.TQ_ADMIN_LOGIN)
+        ["Can query "+s.label]: tq("get","constituents",{
+            query: {"constituentid":"1"},
+            login: s.value + "|" + env.TQ_ADMIN_LOGIN})
             .then(() => true)
             .catch((e) => stringify(e))
         }})
@@ -34,7 +36,11 @@ export const load: PageServerLoad = ({fetch}) => {
     return {
         "AZURE_KEY_VAULT defined": env.AZURE_KEY_VAULT ? true : 
             "The AZURE_KEY_VAULT environment variable must be set",
-        "Azure key vault access": (async () => new Azure().client.listPropertiesOfSecrets().next())()
+        "TQ_ADMIN_LOGIN defined": env.TQ_ADMIN_LOGIN ? true : 
+            "The TQ_ADMIN_LOGIN environment variable must be set",        
+        "PUBLIC_SERVERS defined": env_public.PUBLIC_SERVERS ? true : 
+            "The PUBLIC_SERVERS environment variable must be set",
+        "Azure key vault can be accessed": (async () => new Azure().client.listPropertiesOfSecrets().next())()
             .then(() => true)
             .catch((e) => stringify(e)),
         "tq exists": fs.stat("bin/tq").then(() => true).catch((e) => JSON.parse(JSON.stringify(e))),
@@ -42,13 +48,6 @@ export const load: PageServerLoad = ({fetch}) => {
             let tq = await child_process.spawnSync("bin/tq")
             if(tq.error) throw(tq.error)
             })().then(() => true).catch((e) => stringify(e)),
-        "TQ_ADMIN_LOGIN defined": env.TQ_ADMIN_LOGIN ? true : 
-            "The TQ_ADMIN_LOGIN environment variable must be set",        
-        "TQ_ADMIN_LOGIN is defined in the key store": tq("auth","list")
-            .then((l: string) => l.includes(env.TQ_ADMIN_LOGIN))
-            .catch((e) => stringify(e)),
-        "PUBLIC_SERVERS defined": env_public.PUBLIC_SERVERS ? true : 
-        "The PUBLIC_SERVERS environment variable must be set",
         ...serverTestsObj
         }
 
