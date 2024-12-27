f1ac90be02bf5871050477971eee97c08cccf1d9
skysyzygy <54546783+skysyzygy@users.noreply.github.com>
2024-12-26 21:16:26 -0500

    Implement headers passing via tq()

--- src/lib/tq.ts
+++ src/lib/tq.ts
@@ -2,15 +2,20 @@ import child_process from 'child_process'
 import { tq_key_vault_url } from './config.server';
 import { env } from '$env/dynamic/private'
 
-export async function tq(verb: string, object: string, options?: {variant?: string, query?: any, login?: string, headers?: any}): Promise<any> {
-    let flag = "";
+export async function tq(verb: string, object: string, options?: {variant?: string, query?: any, login?: string, headers?: Record<string,string>}): Promise<any> {
+    let flag = ""
+    let headersString = ""
     if (options?.variant) {
         flag = "--"+options.variant;
     }
+    if (options?.headers) {
+        headersString = "--headers "+Object.entries(options.headers).map((key,val) => `${key}=${val}`).join(",")
+    }
+
     console.log(`running tq (${verb} ${object} ${JSON.stringify(options)})`)
 
     const tqExecutable = (env.OS || "").match(/Windows/i) ? 'bin/tq.exe' : 'bin/tq'
-    var tq = child_process.spawn(tqExecutable, ["-c", "--no-highlight", verb, object, flag], 
+    var tq = child_process.spawn(tqExecutable, ["-c", "--no-highlight", headersString, verb, object, flag], 
     {
         env: {"TQ_LOGIN": options?.login ?? "",
               "AZURE_KEY_VAULT": tq_key_vault_url
