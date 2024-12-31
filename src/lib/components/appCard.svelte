7e4cb254b0e135ae96d4f8b986b0f5d626730154
skysyzygy <54546783+skysyzygy@users.noreply.github.com>
2024-12-30 22:19:41 -0500

    Make the settings button open the form

--- src/lib/components/appCard.svelte
+++ src/lib/components/appCard.svelte
@@ -2,21 +2,23 @@
     import * as Card from "./ui/card"
     import { Button } from "./ui/button"
     import { Settings } from "lucide-svelte"
-    import { Switch } from "./ui/switch"
     import { cn } from "../utils"
-    let { title, children, disabled = false, class: className = undefined } = $props()
-    var blur = $derived(
-        disabled ? "blur-sm opacity-50" : ""
-    )
+	import { Dialog } from "./ui/dialog";
+    let { title, children, config, data, class: className = undefined } = $props()
+    let open = $state(false)
 </script>
 
-<Card.Root class={cn("flex flex-col border-primary shadow-3xl shadow-primary",blur,className)}>
+<Card.Root class={cn("flex flex-col border-primary shadow-3xl shadow-primary",className)}>
     <div class="w-full rounded-t bg-secondary p-3 font-extralight tracking-tight">{title}</div>
     <Card.CardContent class="grow flex flex-col">{@render children()}</Card.CardContent>
     <div class="text-right">
-        <Switch bind:checked={disabled} />
         <Button size="icon" variant="ghost" class="text-secondary-foreground border-primary border-r-0 border-b-0">
-            <Settings class="w-5 h-5"/>
+            <Settings class="w-5 h-5" onclick = {() => {open = true}}/>
         </Button>
     </div>
 </Card.Root>
+{#await data then data}
+{#if typeof config.form === "function"}
+<config.form {data} bind:open = {open}/>
+{/if}
+{/await}
