9bf842959ccac50dd9f28025b6a5bac73aea022a
skysyzygy <54546783+skysyzygy@users.noreply.github.com>
2024-12-30 22:19:03 -0500

    Format the tessi AppCcard

--- src/lib/apps/tessitura/tessituraCard.svelte
+++ src/lib/apps/tessitura/tessituraCard.svelte
@@ -1,18 +1,25 @@
 <script lang="ts">
-	import type { Serializable } from '$lib/apps';
+	import Signup from '$lib/components/signup.svelte';
     import { Badge } from '$lib/components/ui/badge'
     import * as ERRORS from '$lib/errors'
 	import type { TessituraAppLoad } from './tessitura';
     let { data } : { data: Promise<TessituraAppLoad> } = $props()
+    import { servers } from '$lib/const';
 </script>
 
 {#await data}
     <Badge class="mw-min ml-auto animate-pulse">Loading...</Badge>
 {:then tessi}
-    <Badge class="mw-min ml-auto bg-green-600">Connected</Badge>
-    <p>API server: {tessi.tessiApiUrl}</p>
-    <p>Username: {tessi.userid}</p>
-    <p>Group: {tessi.group}</p>
+    {#if tessi.tessiApiUrl}
+        {#if tessi.valid}<Badge class="mw-min ml-auto bg-green-600">Connected</Badge>
+        {:else}<Badge class="mw-min ml-auto bg-destructive">Invalid login</Badge>
+        {/if}
+        <p><span>API server:</span> <span class="text-primary">{servers.filter((v) => v.value === tessi.tessiApiUrl)[0].label}</span></p>
+        <p><span>Username:</span> <span class="text-primary">{tessi.userid}</span></p>
+        <p><span>Group:</span> <span class="text-primary">{tessi.group}</span></p>
+    {:else}
+        <Signup data={tessi}/>
+    {/if}
 {:catch error}
 {#if error.id == ERRORS.USER_NOT_FOUND.id}
     <Badge class="mw-min ml-auto bg-destructive">User not found</Badge>
