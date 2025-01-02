f0dc23370b4f7162f65e45f2db9cec89b0ac46b0
skysyzygy <54546783+skysyzygy@users.noreply.github.com>
2025-01-02 14:36:49 -0500

    rehydrate user after load

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -4,8 +4,6 @@ import { Azure, UserLoaded } from '$lib/azure';
 import type { AppServer } from '$lib/apps.server';
 import { error } from '@sveltejs/kit';
 import * as errors from '$lib/errors'
-import { User } from '$lib/user';
-import { TessituraAppServer } from '$lib/apps/tessitura/tessitura.server';
 
 let servers = new server.AppServers()
 
@@ -19,22 +17,26 @@ function objectMap<I extends any,O extends any>(o: {[k: string]: I},f: (k: strin
 
 export const load: PageServerLoad = async ( { locals }) => {
     let backend = new Azure()
-    let user = backend.load({identity: locals.user.userDetails})
+    let userData = backend.load({identity: locals.user.userDetails})
     let appData = objectMap(servers, 
-        (key, server: AppServer<string,any,any>) => 
-            user.catch(() => new User(locals.user.userDetails) as UserLoaded)
+        (key, server: AppServer<string,any,any,any>) => 
+            userData
+                .then((user) => Object.assign(new UserLoaded(locals.user.userDetails),user))
+                .catch(() => new UserLoaded(locals.user.userDetails))
                 .then((user) => server.load(user, 
                         {identity: user.identity, app: key}))) as AppPromises
-    return {userData: user, appData: appData}
+    return {userData: userData, appData: appData}
 }
 
-const actionFactory = function(key: string,server: AppServer<any,any,any>) {
+const actionFactory = function(key: string,server: AppServer<any,any,any,any>) {
     return async ({request, locals}: RequestEvent) => {
         let formData = await request.formData()
         let backend = new Azure()
-        let user = await backend.load({identity: locals.user.userDetails}).catch(() => new UserLoaded(locals.user.userDetails))
+        let user = await backend.load({identity: locals.user.userDetails})
+            .then((user) => Object.assign(new UserLoaded(locals.user.userDetails),user))
+            .catch(() => new UserLoaded(locals.user.userDetails))
         return server.save(formData, user, {identity: locals.user.userDetails, app: key})
-                    .catch(() => error(500, errors.AZURE_KEYVAULT))
+            .catch(() => error(500, errors.AZURE_KEYVAULT))
 }}
 
 export const actions: Actions = objectMap(servers, (key, server) => actionFactory(key,server))
