5ee58484a816d413f80e9de0ff94019f0fd12cc6
skysyzygy <54546783+skysyzygy@users.noreply.github.com>
2025-01-03 13:12:22 -0500

    Make sure that apps loads the latest data before saving!

--- src/lib/baseapp.server.ts
+++ src/lib/baseapp.server.ts
@@ -1,6 +1,6 @@
 import type { AppServer } from "$lib/apps.server"
 import type { App, AppLoad } from "$lib/apps"
-import { UserLoaded, type BackendKey } from "$lib/azure"
+import { UserLoaded, type ValidBackendKeys } from "$lib/azure"
 import type { ActionFailure } from "@sveltejs/kit"
 import type { Apps } from "./config"
 
@@ -14,13 +14,15 @@ export abstract class BaseAppServer<A extends App<keyof Apps,any,any>, Save exte
     }
 
     // Assigns data into this.data and also returns it
-    async load(backend: UserLoaded, key: BackendKey = {identity: backend.identity, app: this.key}): Promise<AppLoad<A>> {
+    async load(backend: UserLoaded, key: ValidBackendKeys = {identity: backend.identity, app: this.key}): Promise<AppLoad<A>> {
         Object.assign(this.data,backend.load({identity: key.identity, app: this.key}))
         return this.data
     }
 
     // Assigns data into this.data and then saves it to the backend
-    async save(data: Save, backend: UserLoaded, key: BackendKey = {identity: backend.identity, app: this.key}): Promise<void | ActionFailure<any> | {form: any}> {
+    async save(data: Save, backend: UserLoaded, key: ValidBackendKeys = {identity: backend.identity, app: this.key}): Promise<void | ActionFailure<any> | {form: any}> {
+        // Get the freshest data from the backend first
+        Object.assign(this.data, backend.load(key))
         Object.assign(this.data, data)
         return backend.save({identity: key.identity, app: this.key}, this.data)
     }
