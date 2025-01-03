78dc8126a4171994035fa6c9e5bc4ea1554479f5
skysyzygy <54546783+skysyzygy@users.noreply.github.com>
2025-01-03 12:00:35 -0500

    Fix constructors on test page

--- src/routes/tessitura/login/+page.server.ts
+++ src/routes/tessitura/login/+page.server.ts
@@ -3,12 +3,13 @@ import { Azure, UserLoaded } from "$lib/azure"
 import { User } from "$lib/user"
 import type { Actions } from "@sveltejs/kit"
 import type { PageServerLoad } from "./$types"
-import type { TessituraAppSave } from "$lib/apps/tessitura/tessitura"
+import { TessituraApp, type TessituraAppSave } from "$lib/apps/tessitura/tessitura"
+import { serialize } from "$lib/apps"
 
 export const load: PageServerLoad = async ({locals}) => {
     let backend = new Azure()
     let user = await backend.load({identity: locals.user.userDetails}).catch(() => new User(locals.user.userDetails) as UserLoaded)
-    let tessi = new TessituraAppServer()
+    let tessi = new TessituraAppServer(serialize(new TessituraApp()))
     return await tessi.load(user)
 }
 
@@ -17,7 +18,7 @@ export const actions: Actions = {
         let formData = await request.formData()
         let backend = new Azure()
         let user = await backend.load({identity: locals.user.userDetails}).catch(() => new User(locals.user.userDetails) as UserLoaded)
-        let tessi = new TessituraAppServer()
+        let tessi = new TessituraAppServer(serialize(new TessituraApp()))
         return await tessi.save(formData as any, user)    
     } 
 }
