692bd57ef00a24d85a1e6717024bb88121219d3e
skysyzygy <54546783+skysyzygy@users.noreply.github.com>
2025-01-03 20:22:33 -0500

    Will need to figure out how to rollup planStep

--- functions/src/functions/httpTrigger.ts
+++ functions/src/functions/httpTrigger.ts
@@ -1,16 +1,29 @@
 import { app, HttpRequest, HttpResponseInit, InvocationContext } from "@azure/functions";
+import axios from 'axios';
+const baseUrl = "https://agreeable-sky-09447c91e-preview.westus2.4.azurestaticapps.net/"
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0"
 import { planStep } from "../../../src/lib/apps/planStep/planStep.server"
 
-export async function httpTrigger(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
+export async function http(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
     context.log(`Http function processed request for url "${request.url}"`);
+    let url = new URL([request.params.path1, request.params.path2].join("/"),baseUrl).toString()
     console.log(planStep)
-    const name = request.query.get('name') || await request.text() || 'world';
+    let response = await axios<ArrayBuffer>({
+        method: request.method,
+        url: url,
+        // data: request.body,
+        // headers: Object.fromEntries(request.headers.entries())
+    })
 
-    return { body: `Hello, ${name}!` };
+    return { 
+        body: response.data,
+        // headers: Object.fromEntries(Object.entries(response.headers).map(([k,v]) => [k,v]))
+     };
 };
 
 app.http('httpTrigger', {
-    methods: ['GET', 'POST'],
+    methods: ['GET', 'PUT', 'POST'],
     authLevel: 'anonymous',
-    handler: httpTrigger
+    handler: http,
+    route: "tessiverse/{path1:alpha?}/{path2:alpha?}"
 });
