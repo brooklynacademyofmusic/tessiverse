efee7d6767c0659490abf960763edce305f7850a
skysyzygy <54546783+skysyzygy@users.noreply.github.com>
2025-01-04 16:04:37 -0500

    Implement jwt parsing

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,26 +1,47 @@
 import { error, type Handle, redirect } from '@sveltejs/kit';
 import * as errors from "$lib/errors"
 import { env } from "$env/dynamic/private"
-
-export type ClientPrincipal = {
-    identityProvider: string,
-    userId: string,
-    userDetails: string,
-    userRoles: string[]
-} 
+import jwt from "jsonwebtoken"
+import jwks from "jwks-rsa"
 
 export const handle: Handle = async ({ event, resolve }) => {
 
-    const header = Buffer.from(
-        event.request.headers.get('x-ms-client-principal') || 
-        env.DEV_USER ||
-//        event.cookies.get("StaticWebAppsAuthCookie") || 
-        "", 'base64');
-    
-    let user = JSON.parse(header.toString('ascii') || "{}") as ClientPrincipal
-    
+    let user: App.Platform["clientPrincipal"]
+    const token = event.request.headers.get("authorization")?.split(" ")[1]
+    let headers = ""
+    event.request.headers.forEach((v,k) => {headers += `${k}=${v};`})
+    if(token) {
+        error(403, headers)
+        let payload = await new Promise((res,rej) => 
+            jwt.verify(token, getKey, {
+                algorithms: ['RS256'],
+                audience: env.AZURE_CLIENT_ID,
+                issuer: `https://sts.windows.net/${env.AZURE_TENANT_ID}/`
+            },(e,p) => {
+                if(e || !p) rej(e)
+                else res(p)
+            })).catch((e) => error(403,e)) as jwt.JwtPayload | string
+
+        if (typeof payload === "string")
+            payload = JSON.parse(payload) as jwt.JwtPayload
+            
+        user = {
+            identityProvider: "bearer_token",
+            userId: payload.oid,
+            userDetails: payload.upn,
+            userRoles: ["authenticated", "anonymous"].concat(payload.roles)
+        }
+    } else if (event.platform && event.platform.clientPrincipal){
+        user = event.platform.clientPrincipal
+    } else if (env.DEV_USER) {
+        user = JSON.parse(Buffer.from(env.DEV_USER, 'base64').toLocaleString())
+    }
+    if(!user) {
+        user = {identityProvider: "", userId: "", userDetails: "", userRoles: ["anonymous"]}
+    }
+
     event.locals.user = user
-    
+
     if (!("userRoles" in event.locals.user) || !event.locals.user.userRoles.includes("authenticated")) {
         if (!event.request.url.match(/\/login$/)) {
             redirect(302, "/login")
@@ -31,3 +52,19 @@ export const handle: Handle = async ({ event, resolve }) => {
 	const response = await resolve(event);
 	return response;
 };
+
+// Client for getting the JWT signing key
+const client = new jwks.JwksClient({
+    jwksUri: `https://login.microsoftonline.com/${env.AZURE_TENANT_ID}/discovery/keys`
+})
+
+// Helper function to retrieve signing key from kid (key ID)
+const getKey: jwt.GetPublicKeyOrSecret = (header, callback) => {
+    client.getSigningKey(header.kid, (err, key) => {
+        if (err || !key) {
+            return callback(err);
+        }
+        const signingKey = key.getPublicKey();
+        callback(null, signingKey);
+    });
+}
