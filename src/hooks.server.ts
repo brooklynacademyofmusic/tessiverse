9e4a5fb62c600d7a3f2619ee06b0ae93748b6ebc
skysyzygy <54546783+skysyzygy@users.noreply.github.com>
2025-01-05 13:37:35 -0500

    Disabling bearer token-based auth because it doesn't work in Azure SWA, elevating api-key above dev for easier testing

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -11,8 +11,6 @@ export const handle: Handle = async ({ event, resolve }) => {
 
     if (event.platform && event.platform.clientPrincipal){
         user = event.platform.clientPrincipal
-    } else if (env.DEV_USER) {
-        user = JSON.parse(Buffer.from(env.DEV_USER, 'base64').toLocaleString())
     } else if (event.request.headers.get("Api-Key") == env.API_KEY) {
         user = {
             identityProvider: "api_key",
@@ -20,26 +18,28 @@ export const handle: Handle = async ({ event, resolve }) => {
             userDetails: event.request.headers.get("x-ms-workflow-name") || "apiUser",
             userRoles: ["authenticated", "anonymous", "admin"]
         }
-    } else if(token) {
-        let payload = await new Promise((res,rej) => 
-            jwt.verify(token, getKey, {
-                algorithms: ['RS256'],
-                audience: env.AZURE_CLIENT_ID,
-                issuer: `https://sts.windows.net/${env.AZURE_TENANT_ID}/`
-            },(e,p) => {
-                if(e || !p) rej(e)
-                else res(p)
-            })).catch((e) => error(403,e)) as jwt.JwtPayload | string
+    } else if (env.DEV_USER) {
+        user = JSON.parse(Buffer.from(env.DEV_USER, 'base64').toLocaleString())        
+    // } else if(token) {
+    //     let payload = await new Promise((res,rej) => 
+    //         jwt.verify(token, getKey, {
+    //             algorithms: ['RS256'],
+    //             audience: env.AZURE_CLIENT_ID,
+    //             issuer: `https://sts.windows.net/${env.AZURE_TENANT_ID}/`
+    //         },(e,p) => {
+    //             if(e || !p) rej(e)
+    //             else res(p)
+    //         })).catch((e) => error(403,e)) as jwt.JwtPayload | string
 
-        if (typeof payload === "string")
-            payload = JSON.parse(payload) as jwt.JwtPayload
+    //     if (typeof payload === "string")
+    //         payload = JSON.parse(payload) as jwt.JwtPayload
             
-        user = {
-            identityProvider: "bearer_token",
-            userId: payload.oid,
-            userDetails: payload.upn,
-            userRoles: ["authenticated", "anonymous"].concat(payload.roles)
-        }
+    //     user = {
+    //         identityProvider: "bearer_token",
+    //         userId: payload.oid,
+    //         userDetails: payload.upn,
+    //         userRoles: ["authenticated", "anonymous"].concat(payload.roles)
+    //     }
     } 
 
     if(!user) {
