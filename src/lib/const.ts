f9ca0c20f9c83d86c9bc8d99340ceaf7659bddc4
skysyzygy <54546783+skysyzygy@users.noreply.github.com>
2025-02-03 16:09:43 -0500

    Fix quoting on servers definition

--- src/lib/const.ts
+++ src/lib/const.ts
@@ -1,6 +1,6 @@
 import { env } from "$env/dynamic/public"
 
-export const servers = JSON.parse(env.PUBLIC_SERVERS || "['value':'','label':'']") as ServerArray
+export const servers = JSON.parse(env.PUBLIC_SERVERS || '["value":"","label":""]') as ServerArray
 type ServerArray = Array<{value: string, label: string}>
 export type ValidServerValues = {
     [K in keyof typeof servers]: typeof servers[K] extends {value: infer I} ? I : never
