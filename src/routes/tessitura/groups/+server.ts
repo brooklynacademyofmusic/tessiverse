3b52f29d0d7f66445c32df4a0dc20407762eeb7e
skysyzygy <54546783+skysyzygy@users.noreply.github.com>
2025-03-13 08:03:02 -0400

    groups can be cached publicly and persist for a while

--- src/routes/tessitura/groups/+server.ts
+++ src/routes/tessitura/groups/+server.ts
@@ -1,7 +1,8 @@
 import { TessituraAppServer } from "$lib/apps/tessitura/tessitura.server";
 import { json, type RequestHandler } from "@sveltejs/kit";
 
-export const GET: RequestHandler = async () => {
+export const GET: RequestHandler = async ({setHeaders}) => {
+    setHeaders({"cache-control": "max-age=3600, public"})
     return TessituraAppServer.tessiGroups()
-    .then((groups) => json(groups))
+        .then((groups) => json(groups))
 }
