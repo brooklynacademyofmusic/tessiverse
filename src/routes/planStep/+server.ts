f2a3d108dd4e5914330a997a4c005e4e154c3b2a
skysyzygy <54546783+skysyzygy@users.noreply.github.com>
2025-03-13 07:57:47 -0400

    Set content-type to json

--- src/routes/planStep/+server.ts
+++ src/routes/planStep/+server.ts
@@ -1,6 +1,6 @@
 import type { RequestHandler } from './$types';
 import { planStep, type PlanStepEmail } from '$lib/apps/planStep/planStep.server'
-import { error } from '@sveltejs/kit';
+import { error, json } from '@sveltejs/kit';
 import * as ERRORS from '$lib/errors'
 import type { PlanStepAppData } from '$lib/apps/planStep/planStep';
 
@@ -9,8 +9,9 @@ export const POST: RequestHandler = async ({ request, locals }) => {
     if (! locals.user.userRoles.includes("admin") ) {
         error(403, ERRORS.LOGIN)
     }
+    
     return request.json()
-    .then((email: PlanStepEmail) => planStep(email))
-    .then((response: PlanStepAppData["history"][0]) => new Response(JSON.stringify(response)))
+        .then((email: PlanStepEmail) => planStep(email))
+        .then((response) => json(response))
 
 }
